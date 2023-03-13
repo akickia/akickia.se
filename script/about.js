0d820be2bd59c246681628b017f6b541120c4f97
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2023-03-13 11:36:29 +0100

    translated about and changed blur-styling

--- script/about.js
+++ script/about.js
@@ -1,11 +1,21 @@
-let moreEl = document.querySelectorAll(".more")
+let moreEl = document.querySelectorAll(".about .section__more .card")
 
 openMore()
 
 function openMore() {
-    moreEl.forEach(element => {
-      element.addEventListener("click", () => {
-        element.classList.toggle("open")
-      })
-    })
+  let articles = document.querySelectorAll(".more")
+  articles.forEach(element => {
+    let openBtns = element.querySelectorAll(".art_btn")
+    openBtns.forEach(openBtn => {
+      openBtn.addEventListener("click", () => {
+    element.classList.toggle("open")
+    if (element.className.includes("open")) {
+      openBtn.innerHTML = "Läs mindre"
+    }
+    else {
+      openBtn.innerHTML = "Läs mer"
+    }
+
+  })})
+})
 }
